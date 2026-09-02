28c563380373ed48302c9bc15eea5d5b4676a248
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-02 14:10:16 +0900

    Add practical law firm newsletter backfill

--- data/collection.js
+++ data/collection.js
@@ -1,8 +1,8 @@
 window.COLLECTION_META = {
   status: "running",
-  lastCollectedAt: "2026-09-02T12:52:59+09:00",
+  lastCollectedAt: "2026-09-02T13:58:45+09:00",
   intervalMinutes: 60,
-  adoptedCount: 14,
+  adoptedCount: 19,
   scope: "国内企業法務",
   backfill: true
 };
