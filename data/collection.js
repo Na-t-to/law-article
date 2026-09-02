c4e7f3db256190a246ec90db32a5a251e20f920c
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-03 07:52:03 +0900

    Backfill SSBJ disclosure and shareholder meeting reform

--- data/collection.js
+++ data/collection.js
@@ -1,5 +1,5 @@
 window.COLLECTION_STATUS = {
-  lastCollectedAt: "2026-09-03T06:57:00+09:00",
-  adoptedCount: 134,
+  lastCollectedAt: "2026-09-03T07:44:00+09:00",
+  adoptedCount: 143,
   backfill: true
 };
