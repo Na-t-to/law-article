aa183b5a332ee3100b46d77c8810222da47981b7
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-03 02:53:25 +0900

    Backfill digital commerce and work-family support research

--- data/collection.js
+++ data/collection.js
@@ -1,8 +1,8 @@
 window.COLLECTION_META = {
   status: "running",
-  lastCollectedAt: "2026-09-03T01:45:00+09:00",
+  lastCollectedAt: "2026-09-03T02:41:00+09:00",
   intervalMinutes: 60,
-  adoptedCount: 97,
+  adoptedCount: 103,
   scope: "国内企業法務",
   backfill: true
 };
