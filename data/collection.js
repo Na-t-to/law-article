075dd542940e4a33590812d859111358cb65f937
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-02 15:59:08 +0900

    Update LAW INDEX collection metadata after backfill

--- data/collection.js
+++ data/collection.js
@@ -1,8 +1,8 @@
 window.COLLECTION_META = {
   status: "running",
-  lastCollectedAt: "2026-09-02T14:52:22+09:00",
+  lastCollectedAt: "2026-09-02T15:50:25+09:00",
   intervalMinutes: 60,
-  adoptedCount: 25,
+  adoptedCount: 32,
   scope: "国内企業法務",
   backfill: true
 };
