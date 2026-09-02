8331c00f549d76b21f37268a2ce1f780be408440
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-02 12:05:56 +0900

    Backfill enforcement and implementation sources

--- data/collection.js
+++ data/collection.js
@@ -1,8 +1,8 @@
 window.COLLECTION_META = {
   status: "running",
-  lastCollectedAt: "2026-09-02T09:00:00+09:00",
+  lastCollectedAt: "2026-09-02T11:57:22+09:00",
   intervalMinutes: 60,
-  adoptedCount: 6,
+  adoptedCount: 11,
   scope: "国内企業法務",
   backfill: true
 };
