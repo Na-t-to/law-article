cc56ea95a7220ac6df1709710f30a0221f1eb08c
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-02 19:49:43 +0900

    Update LAW INDEX collection status

--- data/collection.js
+++ data/collection.js
@@ -1,8 +1,8 @@
 window.COLLECTION_META = {
   status: "running",
-  lastCollectedAt: "2026-09-02T18:49:00+09:00",
+  lastCollectedAt: "2026-09-02T19:43:00+09:00",
   intervalMinutes: 60,
-  adoptedCount: 54,
+  adoptedCount: 62,
   scope: "国内企業法務",
   backfill: true
 };
