548f5e1e4385d5aeec57e787a5965a4d9f1c56a3
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-02 18:57:06 +0900

    Update LAW INDEX collection metadata

--- data/collection.js
+++ data/collection.js
@@ -1,8 +1,8 @@
 window.COLLECTION_META = {
   status: "running",
-  lastCollectedAt: "2026-09-02T17:53:55+09:00",
+  lastCollectedAt: "2026-09-02T18:49:00+09:00",
   intervalMinutes: 60,
-  adoptedCount: 47,
+  adoptedCount: 54,
   scope: "国内企業法務",
   backfill: true
 };
