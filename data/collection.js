e32a472a869b90b361d82d8f45347639b0bc1a0f
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-03 05:47:45 +0900

    Backfill takeover rules and early business recovery

--- data/collection.js
+++ data/collection.js
@@ -1,5 +1,5 @@
 window.COLLECTION_STATUS = {
-  lastCollectedAt: "2026-09-03T04:49:00+09:00",
-  adoptedCount: 118,
+  lastCollectedAt: "2026-09-03T05:40:00+09:00",
+  adoptedCount: 126,
   backfill: true
 };
