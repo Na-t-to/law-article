08081fb91ad692d415b0c9c1698fec268a546002
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-03 12:00:39 +0900

    Backfill safety, GX ETS, and AML compliance

--- data/collection.js
+++ data/collection.js
@@ -1,5 +1,5 @@
 window.COLLECTION_STATUS = {
-  lastCollectedAt: "2026-09-03T10:44:00+09:00",
-  adoptedCount: 160,
+  lastCollectedAt: "2026-09-03T11:47:00+09:00",
+  adoptedCount: 167,
   backfill: true
 };
