f5a6086c5e50c51fa1b8fe914f79b59bfd89f1be
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-04 20:05:28 +0900

    Add serious accident crisis management shelf

--- data/collection.js
+++ data/collection.js
@@ -1,5 +1,5 @@
 window.COLLECTION_STATUS = {
-  lastCollectedAt: "2026-09-03T14:53:00+09:00",
-  adoptedCount: 179,
+  lastCollectedAt: "2026-09-04T19:51:00+09:00",
+  adoptedCount: 182,
   backfill: true
 };
