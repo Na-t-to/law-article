a51568e70069ff5b4db4343c292c124b54f3ee3f
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-18 18:30:39 +0900

    Link latest APPI practical newsletter

--- data/reforms-run191.js
+++ data/reforms-run191.js
@@ -11,5 +11,5 @@
   pushUnique(event.matchSourceIds, "source-ppc-privacy-rulemaking-basic-1-2026");
   pushUnique(event.sourceIds, "source-ppc-privacy-rulemaking-basic-1-2026");
   pushUnique(event.articleIds, "article-ppc-privacy-rulemaking-basic-1-2026");
-  pushUnique(event.articleIds, "article-miyake-privacy-rules-20260917");
+  pushUnique(event.articleIds, "article-miyake-privacy-rules-20260918");
 })();
