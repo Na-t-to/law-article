c7fa7e31f9407ef93bf66b66c01d03b6f0d76b40
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-21 01:56:51 +0900

    Load AI civil liability normalization patch

--- data/manifest.js
+++ data/manifest.js
@@ -7,3 +7,4 @@ window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,arti
 window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,articles:Object.freeze([...window.LAW_INDEX_DATA_FILES.articles,"articles-run232.js?v=1"])});
 
 window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,articles:Object.freeze([...window.LAW_INDEX_DATA_FILES.articles,"articles-run233.js?v=1"])});
+window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,articles:Object.freeze([...window.LAW_INDEX_DATA_FILES.articles,"articles-run234.js?v=1"])});
