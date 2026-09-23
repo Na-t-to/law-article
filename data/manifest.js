2d69f1d0650418d0be21149b6127d287b92e2a75
Na-t-to <bsk_bakasa@yahoo.co.jp>
2026-09-23 23:19:56 +0900

    Load LAW INDEX run243 data

--- data/manifest.js
+++ data/manifest.js
@@ -19,3 +19,4 @@ window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,arti
 
 window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,articles:Object.freeze([...window.LAW_INDEX_DATA_FILES.articles,"articles-run241.js?v=1"])});
 window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,articles:Object.freeze([...window.LAW_INDEX_DATA_FILES.articles,"articles-run242.js?v=1"])});
+window.LAW_INDEX_DATA_FILES = Object.freeze({...window.LAW_INDEX_DATA_FILES,articles:Object.freeze([...window.LAW_INDEX_DATA_FILES.articles,"articles-run243.js?v=1"])});
